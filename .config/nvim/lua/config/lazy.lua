0f7791644f24e402730f6482913385705cb15e4b
Shihao Tian <shihao.tian@outlook.com>
2026-08-24 23:57:12 +0800

    Update lazy.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -61,6 +61,64 @@ require("lazy").setup({
 						vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 				end,
 		},
+    {
+      'nvim-treesitter/nvim-treesitter',
+      lazy = false,
+      build = ':TSUpdate',
+      config = function()
+        require("nvim-treesitter").setup({
+          ensure_installed = { "markdown", "markdown_inline", "lua", "vim", "vimdoc" },
+          auto_install = true,
+        })
+      end,
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
+      ft = { "markdown", "telekasten" },   -- telekasten 笔记也是 markdown，必须一起加载
+      config = function()
+        -- 先 setup mini.icons，render-markdown 才能拿到图标
+        require("mini.icons").setup()
+        -- 让 treesitter 把 telekasten 文件当 markdown 解析，render 才有 parser 可用
+        vim.treesitter.language.register("markdown", "telekasten")
+        require("render-markdown").setup({
+          heading = { enabled = true },
+          code = { enabled = true },
+          checkbox = { enabled = true },
+          table = { enabled = true },
+        })
+      end,
+    },
+		{
+				"keaising/im-select.nvim",
+				config = function()
+						local is_windows = vim.fn.has("win32") == 1
+						local is_mac     = vim.fn.has("macunix") == 1
+
+						local opts = {
+								set_default_events = { "InsertLeave", "CmdlineLeave" },
+								set_previous_events = { "InsertEnter" },
+								keep_quiet_on_no_binary = false,
+								async_switch_im = true,
+						}
+
+						if is_windows then
+								-- Windows 英文键盘 = 1033
+								opts.default_im_select = "1033"
+								-- 只在此平台指向 im-select.exe 完整路径（不必加入 PATH）
+								opts.default_command = "C:\\Users\\shihao.tian\\im-select.exe"
+						elseif is_mac then
+								-- macOS 用默认 macism，无需 default_command
+								opts.default_im_select = "com.apple.keylayout.ABC"
+						else
+								-- Linux 用默认 fcitx5-remote / fcitx-remote / ibus
+								opts.default_im_select = "xkb:us::eng" -- ibus
+						end
+
+						require("im_select").setup(opts)
+				end,
+		}
+
   },
   install = { colorscheme = { "night-owl" } },
   checker = { enabled = false },
